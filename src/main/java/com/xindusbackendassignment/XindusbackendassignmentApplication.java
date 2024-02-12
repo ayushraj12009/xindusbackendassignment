268fdf6f3e5fd6127ee02030448c21f95c45bce3
ayushraj12009 <ayushraj12009@gmail.com>
2024-02-12 17:13:39 +0530

    Initial Commit

---
+++ src/main/java/com/xindusbackendassignment/XindusbackendassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.xindusbackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XindusbackendassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XindusbackendassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xindusbackendassignment/XindusbackendassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xindusbackendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XindusbackendassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
